b6a9f4968d3237775d0350e2300eff8064f2c6a7
Serhii <nikiforenko.sergiy@gmail.com>
2023-08-26 12:29:07 +0300

    Input Validation - Global Pipes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class CreateUserDto {
 
     @ApiProperty({example: 'user@gmail.com', description: 'Mailing address'})
+    @IsString({message: 'Must be a string'})
+    @IsEmail({}, {message: "Incorrect email"})
     readonly email: string;
     @ApiProperty({example: '12345', description: 'Password'})
+    @IsString({message: 'Must be a string'})
+    @Length(4, 16, {message: 'Not less than 4 and not more than 16'})
     readonly password: string;
 }
