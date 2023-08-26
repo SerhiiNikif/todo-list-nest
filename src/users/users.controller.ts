bf46548f7153f9f2296fd7691fddffb21307117c
Serhii <nikiforenko.sergiy@gmail.com>
2023-08-26 12:10:12 +0300

    Restrict access to an endpoint without a specific user role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @ApiTags('Users')
 @Controller('users')
@@ -20,7 +21,8 @@ export class UsersController {
 
     @ApiOperation({summary: 'Get all users'})
     @ApiResponse({status: 200, type: [User]})
-    @UseGuards(JwtAuthGuard)
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.userService.getAllUsers();
