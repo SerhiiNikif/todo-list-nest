c0c7bcc78945410587e742c559d83a2ffd8e3394
Serhii <nikiforenko.sergiy@gmail.com>
2023-08-26 12:20:39 +0300

    Issuance of roles. User ban

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,18 +5,20 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
 import { Roles } from 'src/auth/roles-auth.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
 
-    constructor(private userService: UsersService) {}
+    constructor(private usersService: UsersService) {}
 
     @ApiOperation({summary: 'User creation'})
     @ApiResponse({status: 200, type: User})
     @Post()
     create(@Body() userDto: CreateUserDto) {
-        return this.userService.createUser(userDto);
+        return this.usersService.createUser(userDto);
     }
 
     @ApiOperation({summary: 'Get all users'})
@@ -25,6 +27,24 @@ export class UsersController {
     @UseGuards(RolesGuard)
     @Get()
     getAll() {
-        return this.userService.getAllUsers();
+        return this.usersService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Выдать роль'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto);
+    }
+
+    @ApiOperation({summary: 'Ban user'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto);
     }
 }
